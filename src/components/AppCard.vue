515942f7bfe69acac32b79286a16ca5e40268328
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-11 18:39:18 +0100

    associazione bandiera alla lingua

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -4,6 +4,13 @@ export default{
 
     props:{
         movie: Object,
+    },
+
+    methods: {
+        getLanguageFlagUrl(languageCode) {
+            // Costruisci l'URL della bandiera usando il codice della lingua
+            return `https://flagcdn.com/w40/${languageCode.toLowerCase()}.png`;
+        }
     }
 }
 </script>
@@ -19,7 +26,8 @@ export default{
             {{ movie.original_title }}
         </div>
         <div class="lang">
-            Lingua: {{ movie.original_language }}
+            Lingua: 
+            <img :src="getLanguageFlagUrl(movie.original_language)" :alt="movie.original_language">
         </div>
         <div class="rating">
             Voto: {{ movie.vote_average }}
@@ -33,6 +41,12 @@ export default{
 
 .card{
     width: calc(100% / 5 - 24px / 5 * 4);
+
+    .lang{
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
 }
 
 </style>
